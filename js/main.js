b6906cf493b1d9e74d1828f6396951d8db3b3923
Uliana Boiko <clubfate@gmail.com>
2023-11-21 02:05:30 +0200

    Style

--- js/main.js
+++ js/main.js
@@ -10,20 +10,79 @@ const formTagCreate = doc.forms.formTagCreate;
 const addNewTag = formTagCreate.addNewTag;
 const addTagBtn = formTagCreate.addTagBtn;
 
+const formInfo = doc.forms.formInfo;
+const addText = formInfo.addText;
+const addClass = formInfo.addClass;
+const addLink = formInfo.addLink;
+const addSrc = formInfo.addSrc;
+const addStyle = formInfo.addStyle;
+const addTextBtn = formInfo.addTextBtn;
+const addClassBtn = formInfo.addClassBtn;
+const addLinkBtn = formInfo.addLinkBtn;
+const addSrcBtn = formInfo.addSrcBtn;
+const errorLink = doc.querySelector('.error-link');
+const errorSrc = doc.querySelector('.error-src');
+
+const formStyle = doc.forms.formStyle;
+const addStyleBtn = formStyle.addStyleBtn;
+
 selectBtnTag.onclick = function() {
-  const valueTag = tag.value;
   const options = tag.options;
   const index = tag.selectedIndex;
   const label = options[index].label;
-  const arrTag = label.split(',');
 
-  if (label.includes('/')) {
-    display.innerHTML = `<${arrTag[0]}><${arrTag[1]}>`;
-  } else {
-    display.innerHTML = `<${arrTag[0]}>`;
+  const newTag = doc.createElement(label);
+
+  display.append(newTag);
+
+  const lastAddedTag = display.lastElementChild;
+
+  addTextBtn.onclick = function() {
+    const valueText = addText.value;
+    lastAddedTag.innerHTML = valueText;  
+    formInfo.reset();
   }
 
-  console.log(display);
+  addClassBtn.onclick = function() {
+    const valueClass = addClass.value;
+    lastAddedTag.className = `${valueClass}`;
+    formInfo.reset();
+  }
+
+  addLinkBtn.onclick = function() {
+    const valueLink = addLink.value;
+
+    if (lastAddedTag.tagName.toLowerCase() === 'a') {
+      lastAddedTag.href = `${valueLink}`;
+      errorLink.style.display = 'none';
+    } else {
+      errorLink.style.display = 'contents';
+    }
+
+    formInfo.reset();
+  }
+
+  addSrcBtn.onclick = function() {
+    const valueSrc = addSrc.value;
+
+    if (lastAddedTag.tagName.toLowerCase() === 'img') {
+      lastAddedTag.src = `${valueSrc}`;
+      errorSrc.style.display = 'none';
+    } else {
+      errorSrc.style.display = 'contents';
+    }
+
+    formInfo.reset();
+  }
+
+  addStyleBtn.onclick = function() {
+    const valueStyle = addStyle.value;
+    lastAddedTag.style.cssText = valueStyle;
+
+    formStyle.reset();
+  }
+
+  formInfo.reset();
 }
 
 addTagBtn.onclick = function() {
@@ -35,4 +94,12 @@ addTagBtn.onclick = function() {
 }
 
 
-console.log(tag[0].value);
+
+
+
+
+
+console.log();
+
+
+
